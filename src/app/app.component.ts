42c03243b34aaf304566a034f7781e8b573bbbb8
Inver <jankowskipawelxd@gmail.com>
2020-11-28 17:34:30 +0100

    Create display element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { DisplayComponent } from './display/display.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  expression = '';
+}
