cdf2e5a76c0080bbcdcc23a36ca862bd5e2786a8
Inver <jankowskipawelxd@gmail.com>
2020-11-28 17:50:06 +0100

    Create Controls component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DisplayComponent } from './display/display.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
-  declarations: [AppComponent, DisplayComponent],
+  declarations: [AppComponent, DisplayComponent, ControlsComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
