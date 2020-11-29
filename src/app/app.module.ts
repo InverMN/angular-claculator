bd69d9dc4b25ced7fb81cf46dd1f8613ed343381
Inver <jankowskipawelxd@gmail.com>
2020-11-29 12:53:43 +0100

    Install FlexLayout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { DisplayComponent } from './display/display.component';
@@ -8,8 +9,13 @@ import { ControlsComponent } from './controls/controls.component';
 import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [AppComponent, DisplayComponent, ControlsComponent, ButtonComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    DisplayComponent,
+    ControlsComponent,
+    ButtonComponent,
+  ],
+  imports: [BrowserModule, FormsModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
